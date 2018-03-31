d0b463f65816f108b59ba8c149780d706e2d675a
juliojsb <jsb.inform@gmail.com>
2018-03-31 19:20:18 +0200

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # jota-jboss-deployer
-Jboss deployer using Python Requests library
+
+Deploy to remote **Jboss EAP6.x+** via HTTP management API using the great **Python Requests library**. I decided to write it in Python mainly because its multiplatform, so you can launch the script in UNIX/Linux, MacOS or even Windows.
+
+Written in Python 3
+
+The usage is very easy, the script takes 4 arguments. Here is an example:
+```
+./jota-standalone-deployer.py admin admin01 http://192.168.2.131:9990 /repository/packages/example.war
+```
+Where:
+
+* admin is the name of the user with admin privileges
+* admin01 is the password for that user
+* http://192.168.2.131:9990 is the destination management URL
+* /repository/packages/example.war is the location of the package in local machine
+
+Once launched, **example.war** should be deployed in the remote Jboss node.
---
+++ jota-standalone-deployer.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import ntpath
+import requests
+import json
+from requests.auth import HTTPDigestAuth
+
+
+#
+# GLOBAL VARIABLES
+#
+
+user = sys.argv[1]
+password = sys.argv[2]
+destination = sys.argv[3]
+full_path = sys.argv[4]
+
+if os.name == 'nt':
+    package = os.path.basename(sys.argv[4])
+
+if os.name == 'posix':
+    package = ntpath.basename(sys.argv[4])
+
+v_bytes_value = ""
+
+
+#
+# FUNCTIONS
+#
+
+def add_content():
+    files = {
+        'file': (full_path, open(full_path, 'rb')),
+    }
+
+    r = requests.post(destination+'/management/add-content', files=files, auth=HTTPDigestAuth(user, password))
+    #print (r.text)
+    #print (r.headers['content-type'])
+    json_data = json.loads(r.text)
+    v_bytes_value = json_data['result']['BYTES_VALUE']
+    #print (v_bytes_value)
+    print ("> Package name {}".format(package))
+    print ("> Added content with hash \"{}\"".format(v_bytes_value))
+    do_deployment(v_bytes_value)
+
+def do_deployment(v_bytes_value):
+    headers = {
+        'Content-Type': 'application/json', 'Accept': 'text/plain'
+    }
+
+    data = '{"content":[{"hash": {"BYTES_VALUE" : "%s"}}], \
+    "address": [{"deployment":"%s"}], \"operation":"add", "enabled":"true"}' % (v_bytes_value,package)
+       
+    #print (json.dumps(data))
+    print ("> Deploying package {} to node {}".format(package,destination))
+    print ("> Result:")
+    
+    response = requests.post(destination+'/management', auth=HTTPDigestAuth(user, password), \
+                             headers=headers, data=(data))
+
+    #print response
+    print response.text
+
+    
+#
+# MAIN
+#
+ 
+if __name__ == '__main__':
+    add_content()
