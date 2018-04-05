7f3cf1b5c8f6224b2f1f9a4ea78a4d864895f3ce
juliojsb <jsb.inform@gmail.com>
2018-04-05 21:47:40 +0200

    Added argument parser

--- jota-domain-deployer.py
+++ jota-domain-deployer.py
@@ -8,78 +8,66 @@ License       :GPLv3
 """
 
 import os
-import sys
+import argparse
 import ntpath
 import requests
 import json
 from requests.auth import HTTPDigestAuth
 
 
-#
-# GLOBAL VARIABLES
-#
+def do_deploy(args):
+    
+    user = args.user
+    password = args.password
+    destination = args.destination
+    server_group = args.servergroup
+    full_path = args.package
 
-user = sys.argv[1]
-password = sys.argv[2]
-destination = sys.argv[3]
-full_path = sys.argv[4]
-server_group = sys.argv[5]
-
-if os.name == 'nt':
-    package = os.path.basename(sys.argv[4])
-
-if os.name == 'posix':
-    package = ntpath.basename(sys.argv[4])
-
-v_bytes_value = ""
-
-
-#
-# FUNCTIONS
-#
-
-def add_content():
+    if os.name == 'nt':
+        package = os.path.basename(full_path)
+    if os.name == 'posix':
+        package = ntpath.basename(full_path)
+    
     files = {
         'file': (full_path, open(full_path, 'rb')),
     }
-
-    r = requests.post(destination+'/management/add-content', files=files, auth=HTTPDigestAuth(user, password))
-    #print (r.text)
-    #print (r.headers['content-type'])
-    json_data = json.loads(r.text)
-    v_bytes_value = json_data['result']['BYTES_VALUE']
-    #print (v_bytes_value)
-    print ("> Package name {}".format(package))
-    print ("> Added content with hash \"{}\"".format(v_bytes_value))
-    do_deployment(v_bytes_value)
-
-def do_deployment(v_bytes_value):
     headers = {
         'Content-Type': 'application/json', 'Accept': 'text/plain'
     }
 
+    r_content = requests.post(destination+'/management/add-content', files=files, auth=HTTPDigestAuth(user, password))
+    json_data = json.loads(r_content.text)
+    v_bytes_value = json_data['result']['BYTES_VALUE']
+    print ("> Package name {}".format(package))
+    print ("> Added content with hash \"{}\"".format(v_bytes_value))
+
     data_repository = '{"content":[{"hash": {"BYTES_VALUE" : "%s"}}], \
     "address": [{"deployment":"%s"}], \"operation":"add"}' % (v_bytes_value,package)
-
     data_deploy = '{"content":[{"hash": {"BYTES_VALUE" : "%s"}}], \
     "address": [{"server-group":"%s"},{"deployment":"%s"}], \"operation":"add", "enabled":"true"}' % (v_bytes_value,server_group,package)
 
     # Add package to repository
-    print ("> Added package {} to Repository".format(package))
-    response = requests.post(destination+'/management', auth=HTTPDigestAuth(user, password), \
-                             headers=headers, data=(data_repository))
+    print ("> Added package {} to repository".format(package))
+    requests.post(destination+'/management', auth=HTTPDigestAuth(user, password), headers=headers, data=(data_repository))
 
     # Deploy (enable) package
     print ("> Deploying package {} to Server Group {}".format(package,server_group))
-    response = requests.post(destination+'/management', auth=HTTPDigestAuth(user, password), \
+    r_deploy = requests.post(destination+'/management', auth=HTTPDigestAuth(user, password), \
                              headers=headers, data=(data_deploy))
     print ("> Result:")
-    print response.text
+    print (r_deploy.text)
 
-    
-#
-# MAIN
-#
  
 if __name__ == '__main__':
-    add_content()
+    # Initial argument parser
+    parser = argparse.ArgumentParser(description="Deploy to remote Jboss Server Group (domain mode)")
+    required = parser.add_argument_group("Required arguments")
+    required.add_argument('-u', '--user', required=True, help='Jboss user with administrative privileges')
+    required.add_argument('-p', '--password', required=True, help='Password for admin user')
+    required.add_argument('-d', '--destination', required=True, metavar='http(s)://IP:PORT' , help='Destination IP')
+    required.add_argument('-sg', '--servergroup', required=True, help='Destination Server Group')
+    required.add_argument('-pkg', '--package', required=True, metavar='/path/to/package', help='Full path to package')
+    args = parser.parse_args()
+    
+    # Function calls
+    do_deploy(args)   
--- jota-standalone-deployer.py
+++ jota-standalone-deployer.py
@@ -8,72 +8,62 @@ License       :GPLv3
 """
 
 import os
-import sys
 import ntpath
 import requests
 import json
+import argparse
 from requests.auth import HTTPDigestAuth
 
 
-#
-# GLOBAL VARIABLES
-#
+def do_deploy(args):
+    
+    user = args.user
+    password = args.password
+    destination = args.destination
+    full_path = args.package
 
-user = sys.argv[1]
-password = sys.argv[2]
-destination = sys.argv[3]
-full_path = sys.argv[4]
-
-if os.name == 'nt':
-    package = os.path.basename(sys.argv[4])
-
-if os.name == 'posix':
-    package = ntpath.basename(sys.argv[4])
-
-v_bytes_value = ""
-
-
-#
-# FUNCTIONS
-#
-
-def add_content():
+    if os.name == 'nt':
+        package = os.path.basename(full_path)
+    if os.name == 'posix':
+        package = ntpath.basename(full_path)
+    
     files = {
         'file': (full_path, open(full_path, 'rb')),
     }
-
-    r = requests.post(destination+'/management/add-content', files=files, auth=HTTPDigestAuth(user, password))
-    #print (r.text)
-    #print (r.headers['content-type'])
-    json_data = json.loads(r.text)
-    v_bytes_value = json_data['result']['BYTES_VALUE']
-    #print (v_bytes_value)
-    print ("> Package name {}".format(package))
-    print ("> Added content with hash \"{}\"".format(v_bytes_value))
-    do_deployment(v_bytes_value)
-
-def do_deployment(v_bytes_value):
     headers = {
         'Content-Type': 'application/json', 'Accept': 'text/plain'
-    }
+    }    
 
+    # Add content    
+    r_content = requests.post(destination+'/management/add-content', files=files, auth=HTTPDigestAuth(user, password))
+
+    json_data = json.loads(r_content.text)
+    v_bytes_value = json_data['result']['BYTES_VALUE']
+    print ("> Package name {}".format(package))
+    print ("> Added content with hash \"{}\"".format(v_bytes_value))
+
+    # Enable deployment
     data = '{"content":[{"hash": {"BYTES_VALUE" : "%s"}}], \
     "address": [{"deployment":"%s"}], \"operation":"add", "enabled":"true"}' % (v_bytes_value,package)
        
-    #print (json.dumps(data))
     print ("> Deploying package {} to node {}".format(package,destination))
     print ("> Result:")
     
-    response = requests.post(destination+'/management', auth=HTTPDigestAuth(user, password), \
+    r_deploy = requests.post(destination+'/management', auth=HTTPDigestAuth(user, password), \
                              headers=headers, data=(data))
 
-    #print response
-    print response.text
+    print (r_deploy.text)
 
-    
-#
-# MAIN
-#
- 
+
 if __name__ == '__main__':
-    add_content()
+    # Initial argument parser
+    parser = argparse.ArgumentParser(description="Deploy to remote Jboss standalone node")
+    required = parser.add_argument_group("Required arguments")
+    required.add_argument('-u', '--user', required=True, help='Jboss user with administrative privileges')
+    required.add_argument('-p', '--password', required=True, help='Password for admin user')
+    required.add_argument('-d', '--destination', required=True, metavar='http(s)://IP:PORT' , help='Destination IP')
+    required.add_argument('-pkg', '--package', required=True, metavar='/path/to/package', help='Full path to package')
+    args = parser.parse_args()
+    
+    # Function calls
+    do_deploy(args)   
